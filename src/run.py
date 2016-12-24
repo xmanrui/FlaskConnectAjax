c5ed4ea5952c1182abb0e4e79296e3dd72c2f2cf
xiemanrui@foxmail.com <xiemanrui@foxmail.com>
2016-12-24 14:02:26 +0800

    init: add files

---
+++ src/run.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask import jsonify
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "dfdfdffdad"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/mystring')
+def mystring():
+    return 'my string'
+
+@app.route('/mydict', methods=['GET', 'POST'])
+def mydict():
+    d = {'name': 'xmr', 'age': 18}
+    return jsonify(d)
+
+@app.route('/mylist')
+def mylist():
+    l = ['xmr', 18]
+    return jsonify(l)
+
+if __name__ == '__main__':
+    app.run()
